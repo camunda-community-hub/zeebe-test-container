072a96ebcc61332d514970c257955bdea1026b0f
Gregor B. Rosenauer <gregor.rosenauer@viesure.io>
2020-01-24 15:15:53 +0100

    #21 update for Zeebe 0.22.1 final
    - removed alpha versions, all supported versions are final now
    - WIP: tests run through in IDE, but not on command line -> container timeouts for cluster tests
    - storing version in ZeebeBrokerContainer so we can retrieve the correct log output (stored in Enum now)
    - moved SupportedVersion to make it accessible from both test and implementation

--- src/main/java/io/zeebe/containers/SupportedVersion.java
+++ src/main/java/io/zeebe/containers/SupportedVersion.java
@@ -15,20 +15,39 @@
  */
 package io.zeebe.containers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum SupportedVersion {
-  ZEEBE_0_20_1("0.20.1"),
-  ZEEBE_0_21_1("0.21.1"),
-  ZEEBE_0_22_0_alpha1("0.22.0-alpha1"),
-  ZEEBE_0_22_0_alpha2("0.22.0-alpha2");
+  ZEEBE_0_20_1("0.20.1", ".*Broker is ready.*"),
+  ZEEBE_0_21_1("0.21.1", ".*Broker is ready.*"),
+  ZEEBE_0_22_1("0.22.1", ".* succeeded. Started.*");
 
+  private static final Map<String, SupportedVersion> LOOKUP_MAP = new HashMap<>();
   private final String version;
+  private final String logStringRegex;
 
-  SupportedVersion(final String version) {
+  static {
+    Arrays.stream(SupportedVersion.values())
+        .forEach(version -> LOOKUP_MAP.put(version.version(), version));
+  }
+
+  SupportedVersion(final String version, final String logStringRegex) {
     this.version = version;
+    this.logStringRegex = logStringRegex;
   }
 
   public String version() {
     return version;
+  }
+
+  public String logStringRegex() {
+    return logStringRegex;
+  }
+
+  public static SupportedVersion fromVersion(String version) {
+    return LOOKUP_MAP.get(version);
   }
 
   @Override
